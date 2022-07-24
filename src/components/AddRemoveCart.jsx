66ac1485b6b66c54713e2240439aabb216b71817
Adam Blevins <blevinspercussion@gmail.com>
2022-07-24 10:27:46 -0500

    Basic styling and content for ProductDetail. Created skeleton for AddRemoveCart component that will be added to the bottom of ProductDetail

---
+++ src/components/AddRemoveCart.jsx
@@ -0,0 +1,11 @@
+import './components.css';
+
+function AddRemoveCart () {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AddRemoveCart;
--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,9 +1,21 @@
 import './components.css';
+import AddRemoveCart from './AddRemoveCart';
 
 function ProductDetail () {
     return (
-        <div>
-
+        <div className='product-detail'>
+            <h6 className='product-head'>SNEAKER COMPANY</h6>
+            <h1 className='product-title'><strong>Fall Limited Edition Sneakers</strong></h1>
+            <p>
+                These low-profile sneakers are your perfect casual wear 
+                companion. Featuring a durable rubber outer sole, they'll 
+                withstand everything the weather can offer.
+            </p>
+            <div className='current-price'>
+                <h2>$125.00</h2>
+                <h4 className='discount'>50%</h4>
+            </div>
+            <h3 className='old-price'>$250.00</h3>
         </div>
     )
 }
