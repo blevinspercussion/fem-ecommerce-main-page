9b11022c62d1277389be6c31ac9b7d7bfbfd2ae5
Adam Blevins <blevinspercussion@gmail.com>
2022-07-24 09:29:48 -0500

    Nav bar in place...still need to fix spacing on shopping cart/avatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
 import './components.css';
+import logo from './img/logo.svg'
+import shoppingCart from './img/icon-cart.svg';
+import profilePic from './img/image-avatar.png';
 
 function Header () {
     return (
-        <div>
-
+        <div id='header'>
+            <ul className='main-nav'>
+                <li><img src={logo}></img></li>
+                <li><a href='#'>Collections</a></li>
+                <li><a href='#'>Men</a></li>
+                <li><a href='#'>Women</a></li>
+                <li><a href='#'>About</a></li>
+                <li><a href='#'>Contact</a></li>
+                <li className='push'><img src={shoppingCart}></img></li>
+                <li><img className='profile-pic' src={profilePic}></img></li>
+            </ul>
         </div>
     )
 }
