32a78894f32f44a06d565e8411dc917ffce184f1
Adam Blevins <blevinspercussion@gmail.com>
2022-07-25 10:30:34 -0500

    Fixed styling for lightbox arrows

--- src/components/LightBox.jsx
+++ src/components/LightBox.jsx
@@ -1,10 +1,18 @@
 import './components.css';
+import leftArrow from './img/icon-previous.svg';
+import rightArrow from './img/icon-next.svg';
+import closeIcon from './img/icon-close.svg';
 import ProductDisplay from './ProductDisplay';
 
 function LightBox () {
     return (
         <div className='lightbox'>
             <ProductDisplay />
+            <img className='arrow left-arrow fake-arrow' src={leftArrow}></img>
+            <img className='arrow left-arrow' src={leftArrow}></img>
+            <img className='arrow right-arrow' src={rightArrow}></img>
+            <img className='arrow right-arrow fake-arrow' src={rightArrow}></img>
+
         </div>
     )
 }
