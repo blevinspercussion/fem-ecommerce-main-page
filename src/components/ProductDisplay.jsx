93674d6854e030cdf0f83b0b788a8de83be55d54
Adam Blevins <blevinspercussion@gmail.com>
2022-07-26 10:59:38 -0500

    mobile arrows are in place

--- src/components/ProductDisplay.jsx
+++ src/components/ProductDisplay.jsx
@@ -8,6 +8,8 @@ import productImage1Thumbnail from './img/image-product-1-thumbnail.jpg';
 import productImage2Thumbnail from './img/image-product-2-thumbnail.jpg';
 import productImage3Thumbnail from './img/image-product-3-thumbnail.jpg';
 import productImage4Thumbnail from './img/image-product-4-thumbnail.jpg';
+import leftArrow from './img/icon-previous.svg';
+import rightArrow from './img/icon-next.svg';
 
 
 
@@ -17,21 +19,44 @@ function ProductDisplay ({ toggleLightBox, currentPic, handleCurrentPic, thumbna
 
     return (
         <div className='product-display'>
-            <div className='product-main-image'>
-                <img className='product-image product-image-large' src={currentPic} onClick={toggleLightBox}></img>
+            <div className='product-display-desktop'>
+                <div className='product-main-image'>
+                    <img className='product-image product-image-large' src={currentPic} onClick={toggleLightBox}></img>
+                </div>
+                <div className='product-sub-images'>
+                    <div className='product-image-small-bg'>
+                        <img className={'product-image product-image-small selected-image product-image1'} id='productImage1' src={productImage1Thumbnail} onClick={handleSelected}></img>
+                    </div>
+                    <div className='product-image-small-bg'>
+                        <img className={'product-image product-image-small product-image2'} id='productImage2' src={productImage2Thumbnail} onClick={handleSelected}></img>
+                    </div>
+                    <div className='product-image-small-bg'>
+                        <img className={'product-image product-image-small product-image3'} id='productImage3' src={productImage3Thumbnail} onClick={handleSelected}></img>
+                    </div>
+                    <div className='product-image-small-bg'>
+                        <img className={'product-image product-image-small product-image4'} id='productImage4' src={productImage4Thumbnail} onClick={handleSelected}></img>
+                    </div>
+                </div>
             </div>
-            <div className='product-sub-images'>
-                <div className='product-image-small-bg'>
-                    <img className={'product-image product-image-small selected-image product-image1'} id='productImage1' src={productImage1Thumbnail} onClick={handleSelected}></img>
+            <div className='product-display-mobile'>
+            <div className='product-main-image'>
+                    <img className='product-image product-image-large' src={currentPic}></img>
+                    <img className='mobile-left-arrow' src={leftArrow} />
+                    <img className='mobile-right-arrow' src={rightArrow} />
                 </div>
-                <div className='product-image-small-bg'>
-                    <img className={'product-image product-image-small product-image2'} id='productImage2' src={productImage2Thumbnail} onClick={handleSelected}></img>
-                </div>
-                <div className='product-image-small-bg'>
-                    <img className={'product-image product-image-small product-image3'} id='productImage3' src={productImage3Thumbnail} onClick={handleSelected}></img>
-                </div>
-                <div className='product-image-small-bg'>
-                    <img className={'product-image product-image-small product-image4'} id='productImage4' src={productImage4Thumbnail} onClick={handleSelected}></img>
+                <div className='product-sub-images'>
+                    <div className='product-image-small-bg'>
+                        <img className={'product-image product-image-small selected-image product-image1'} id='productImage1' src={productImage1Thumbnail} onClick={handleSelected}></img>
+                    </div>
+                    <div className='product-image-small-bg'>
+                        <img className={'product-image product-image-small product-image2'} id='productImage2' src={productImage2Thumbnail} onClick={handleSelected}></img>
+                    </div>
+                    <div className='product-image-small-bg'>
+                        <img className={'product-image product-image-small product-image3'} id='productImage3' src={productImage3Thumbnail} onClick={handleSelected}></img>
+                    </div>
+                    <div className='product-image-small-bg'>
+                        <img className={'product-image product-image-small product-image4'} id='productImage4' src={productImage4Thumbnail} onClick={handleSelected}></img>
+                    </div>
                 </div>
             </div>
 
