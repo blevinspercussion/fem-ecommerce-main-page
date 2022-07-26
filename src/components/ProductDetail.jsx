1e64beaa9375dc271f421b1b9a81ac92846fdae2
Adam Blevins <blevinspercussion@gmail.com>
2022-07-26 09:56:18 -0500

    Finished basic layout for mobile

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -11,11 +11,13 @@ function ProductDetail ({ totalInCart, setTotalInCart, addToCart, removeFromCart
                 companion. Featuring a durable rubber outer sole, they'll 
                 withstand everything the weather can offer.
             </p>
+            <div className='prices'>
             <div className='current-price'>
                 <h2>$125.00</h2>
                 <h4 className='discount'>50%</h4>
             </div>
             <h3 className='old-price'>$250.00</h3>
+            </div>
             <AddRemoveCart 
                 totalInCart={totalInCart}
                 setTotalInCart={setTotalInCart}
